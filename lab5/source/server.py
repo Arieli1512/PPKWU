a504be6054db7b2217bf41c163747fbf4036911f
Jakub Wlaźlak <45246122+Arieli1512@users.noreply.github.com>
2022-12-01 15:16:22 +0100

    First string stats try

--- lab5/source/server.py
+++ lab5/source/server.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from flask import Flask, request, jsonify
+import re
 
 parameter_names = ["sum", "sub", "mul", "div", "mod"]
 
@@ -7,14 +8,27 @@ app = Flask(__name__)
 
 def calculateInts(num1 :int, num2: int):
     return {"sum": num1 + num2, "sub": num1 - num2, "mul": num1 * num2, "div": num1 // num2, "mod": num1 % num2}
-
+def stringStats(input : str):
+    lowercase = len(re.findall(r'[a-z]', input))
+    uppercase = len(re.findall(r'[A-Z]', input))
+    digits = len(re.findall(r'[1-9]', input))
+    rest = len(input) - lowercase - uppercase - digits
+    return {
+        "lowercase": lowercase,
+        "uppercase": uppercase,
+        "digits": digits,
+        "special": rest
+    }
 @app.route("/", methods=['POST'])
 def get_numbers():
     request_json = request.get_json()
+    inputStr = request_json.get("str")
     num1 = request_json.get("num1")
     num2 = request_json.get("num2")
     if num1 is not None and num2 is not None:
         output = calculateInts(num1, num2)
+    if inputStr is not None:
+        output = stringStats(inputStr)
     return output
 
 
