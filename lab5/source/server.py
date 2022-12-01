116ec2c658639b85ee4a7da6528d45179f86882b
Jakub Wlaźlak <45246122+Arieli1512@users.noreply.github.com>
2022-12-01 15:23:05 +0100

    String + Ints integration

--- lab5/source/server.py
+++ lab5/source/server.py
@@ -19,16 +19,32 @@ def stringStats(input : str):
         "digits": digits,
         "special": rest
     }
+def multipleCalc(num1 :int, num2: int, input : str)
+    return {
+        "lowercase": lowercase,
+        "uppercase": uppercase,
+        "digits": digits,
+        "special": rest,
+        "sum": num1 + num2,
+        "sub": num1 - num2,
+        "mul": num1 * num2,
+        "div": num1 // num2,
+        "mod": num1 % num2      
+    }
+
 @app.route("/", methods=['POST'])
 def get_numbers():
     request_json = request.get_json()
     inputStr = request_json.get("str")
     num1 = request_json.get("num1")
     num2 = request_json.get("num2")
-    if num1 is not None and num2 is not None:
-        output = calculateInts(num1, num2)
-    if inputStr is not None:
-        output = stringStats(inputStr)
+    if num1 is not None and num2 is not None and inputStr is not None:
+        output = multipleCalc(num1,num2,inputStr)
+    else:
+        if num1 is not None and num2 is not None:
+            output = calculateInts(num1, num2)
+        if inputStr is not None:
+            output = stringStats(inputStr)
     return output
 
 
