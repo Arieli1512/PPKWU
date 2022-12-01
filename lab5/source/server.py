99516bcf05de69e452eb73e61568195d523366c0
Jakub Wlaźlak <45246122+Arieli1512@users.noreply.github.com>
2022-12-01 14:49:56 +0100

    First implementation try

--- lab5/source/server.py
+++ lab5/source/server.py
@@ -2,23 +2,44 @@
 import http.server
 import socketserver
 import os
+from urllib.parse import urlparse
+from urllib.parse import parse_qs
+import json
 
 #print('source code for "http.server":', http.server.__file__)
 
 class web_server(http.server.SimpleHTTPRequestHandler):
     
-    def do_GET(self):
-
+    def do_POST(self):
+        parsed = urlparse(self.path)
+        qs = parse_qs(parsed.query)
         print(self.path)
         
-        if self.path == '/':
+        if len(qs) == 2 and 'num1' in qs.keys() and 'num2' in qs.keys():
             self.protocol_version = 'HTTP/1.1'
             self.send_response(200)
-            self.send_header("Content-type", "text/html; charset=UTF-8")
-            self.end_headers()            
-            self.wfile.write(b"Hello World!\n")
+            self.send_header("Content-type", "application/json")
+            self.end_headers() 
+            num1 = int(qs['num1'][0])
+            num2 = int(qs['num2'][0])
+            response = {
+                "sum" : num1+num2,
+                "sub" : num1-num2,
+                "mul" : num1*num2,
+                "div" : num1//num2,
+                "mod" : num1%num2
+            }
+            self.wfile.write(json.dumps(response).encode())
         else:
-            super().do_GET()
+            if self.path =='/':
+                self.protocol_version = 'HTTP/1.1'
+                self.send_response(200)
+                self.send_header("Content-type", "text/html; charset=UTF-8")
+                self.end_headers() 
+                self.wfile.write(b"ERROR\n")
+            else:
+                super().do_GET()
+    
     
 # --- main ---
 
