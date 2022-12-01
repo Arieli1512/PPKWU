9ee808a0bb62765374357eece47d6542611f9ae1
Jakub Wlaźlak <45246122+Arieli1512@users.noreply.github.com>
2022-12-01 15:07:02 +0100

    Remake in flask and first working ints

--- lab5/source/server.py
+++ lab5/source/server.py
@@ -1,51 +1,21 @@
 #!/usr/bin/env python3
-import http.server
-import socketserver
-import os
-from urllib.parse import urlparse
-from urllib.parse import parse_qs
-import json
+from flask import Flask, request, jsonify
 
-#print('source code for "http.server":', http.server.__file__)
+parameter_names = ["sum", "sub", "mul", "div", "mod"]
 
-class web_server(http.server.SimpleHTTPRequestHandler):
-    
-    def do_POST(self):
-        parsed = urlparse(self.path)
-        qs = parse_qs(parsed.query)
-        print(self.path)
-        
-        if len(qs) == 2 and 'num1' in qs.keys() and 'num2' in qs.keys():
-            self.protocol_version = 'HTTP/1.1'
-            self.send_response(200)
-            self.send_header("Content-type", "application/json")
-            self.end_headers() 
-            num1 = int(qs['num1'][0])
-            num2 = int(qs['num2'][0])
-            response = {
-                "sum" : num1+num2,
-                "sub" : num1-num2,
-                "mul" : num1*num2,
-                "div" : num1//num2,
-                "mod" : num1%num2
-            }
-            self.wfile.write(json.dumps(response).encode())
-        else:
-            if self.path =='/':
-                self.protocol_version = 'HTTP/1.1'
-                self.send_response(200)
-                self.send_header("Content-type", "text/html; charset=UTF-8")
-                self.end_headers() 
-                self.wfile.write(b"ERROR\n")
-            else:
-                super().do_GET()
-    
-    
-# --- main ---
+app = Flask(__name__)
 
-PORT = 4080
+def calculateInts(num1 :int, num2: int):
+    return {"sum": num1 + num2, "sub": num1 - num2, "mul": num1 * num2, "div": num1 // num2, "mod": num1 % num2}
 
-print(f'Starting: http://localhost:{PORT}')
+@app.route("/", methods=['POST'])
+def get_numbers():
+    request_json = request.get_json()
+    num1 = request_json.get("num1")
+    num2 = request_json.get("num2")
+    if num1 is not None and num2 is not None:
+        output = calculateInts(num1, num2)
+    return output
 
-tcp_server = socketserver.TCPServer(("",PORT), web_server)
-tcp_server.serve_forever()
+
+app.run(port=4080, host='0.0.0.0')
