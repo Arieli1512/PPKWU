54654a3e14ef90337fdfe36f13cd8482fc2ae965
Jakub Wlaźlak <45246122+Arieli1512@users.noreply.github.com>
2022-12-15 12:20:06 +0100

    Lab6 init

---
+++ lab6/source/server.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+from flask import Flask, request, jsonify
+import re
+
+app = Flask(__name__)
+
+def calculateInts(num1 :int, num2: int):
+    return {"sum": num1 + num2, "sub": num1 - num2, "mul": num1 * num2, "div": num1 // num2, "mod": num1 % num2}
+def stringStats(input : str):
+    lowercase = len(re.findall(r'[a-z]', input))
+    uppercase = len(re.findall(r'[A-Z]', input))
+    digits = len(re.findall(r'[1-9]', input))
+    rest = len(input) - lowercase - uppercase - digits
+    return {
+        "lowercase": lowercase,
+        "uppercase": uppercase,
+        "digits": digits,
+        "special": rest
+    }
+def multipleCalc(num1 :int, num2: int, input : str):
+    lowercase = len(re.findall(r'[a-z]', input))
+    uppercase = len(re.findall(r'[A-Z]', input))
+    digits = len(re.findall(r'[1-9]', input))
+    rest = len(input) - lowercase - uppercase - digits
+    return {
+        "lowercase": lowercase,
+        "uppercase": uppercase,
+        "digits": digits,
+        "special": rest,
+        "sum": num1 + num2,
+        "sub": num1 - num2,
+        "mul": num1 * num2,
+        "div": num1 // num2,
+        "mod": num1 % num2      
+    }
+
+@app.route("/", methods=['POST'])
+def get_numbers():
+    request_json = request.get_json()
+    inputStr = request_json.get("str")
+    num1 = request_json.get("num1")
+    num2 = request_json.get("num2")
+    if num1 is not None and num2 is not None and inputStr is not None:
+        output = multipleCalc(num1,num2,inputStr)
+    else:
+        if num1 is not None and num2 is not None:
+            output = calculateInts(num1, num2)
+        if inputStr is not None:
+            output = stringStats(inputStr)
+    return output
+
+
+app.run(port=4080, host='0.0.0.0')
